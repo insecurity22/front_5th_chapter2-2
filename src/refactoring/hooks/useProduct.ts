0386fa33459f5a0abdd2a43cfd2bc4da7b3d53a8
insecurity22 <insecurity@naver.com>
2025-04-24 04:55:43 +0900

    feat: useProduct hook 코드 작성

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,21 @@ import { useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const addProduct = (product: Product) => {
+    setProducts((prevProducts) => [...prevProducts, product]);
+  };
+
+  const updateProduct = (product: Product) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((p) => (p.id === product.id ? product : p))
+    );
+  };
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
